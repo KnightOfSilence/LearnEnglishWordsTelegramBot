dceb2032754cc252afeeb881830233a0a05bfd06
boss <vodochnik1982@gmail.com>
2025-09-21 22:37:41 +0300

    KTB-06. Если все верно на этом этапе, то дальше в main ветке создам единый файл main.kt и перенесу информацию из всех предыдущих файлов.

--- src/main/kotlin/stage_3/stage3_task6.kt
+++ src/main/kotlin/stage_3/stage3_task6.kt
@@ -3,8 +3,11 @@ package stage_3
 import stage_2.Word
 
 public fun calculateStatistics(dictionary: MutableList<Word>): String {
-    val totalCount = dictionary.map{it.original}.count()
-    val learnedCount = dictionary.filter { it.correctAnswersCount >= 0 }.count()
-    val percent = (learnedCount.toDouble() / totalCount.toDouble()) * 100
-    return "Выучено $learnedCount из $totalCount слов | $percent%"
+    val totalCount = dictionary.size
+    val learnedCount = dictionary.filter { it.correctAnswersCount >= 3 }.count()
+    if (totalCount == 0) return "Словарь пуст"
+    else {
+        val percent = (learnedCount.toDouble() / totalCount.toDouble()) * 100
+        return "Выучено $learnedCount из $totalCount слов | ${"%.1f".format(percent)}%"
+    }
 }
